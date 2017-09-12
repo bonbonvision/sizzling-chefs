1e0c4fd7e2300dfa36eaa5e06d99c9dc6b3f0fda
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-09-12 23:41:00 +0000

    Hooked up create action in chef profile controller

--- app/controllers/chef/profiles_controller.rb
+++ app/controllers/chef/profiles_controller.rb
@@ -3,6 +3,16 @@ class Chef::ProfilesController < ApplicationController
 
   def new
     @profile = Profile.new
+  end
 
+  def create
+    @profile = current_user.profile
+    redirect_to chef_profile_path(@profile)
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:subtitle, :bio, :city, :state, :favorite_dish, :inspiration)
   end
 end
