e17024300188d1a7e7f9591c7d137a01474252fb
bonbonvision <kilpatrick.bonnie@gmail.com>
2017-11-25 15:24:42 +0000

    Chef profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,13 +2,13 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  # def after_sign_in_path_for(resource)
-  #   if resource.role == 'chef'
-  #     redirect_to dashboard_path
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def after_sign_in_path_for(resource)
+    if resource.is_a? Chef
+      dashboard_path
+    else
+      root_path
+    end
+  end
   
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :type, :password, :password_confirmation)
